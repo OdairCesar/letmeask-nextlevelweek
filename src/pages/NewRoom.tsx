7a88c0af81e35031a17ae4ac0deb2a130360157b
Odair Cesar Ferreira <odairferreira97@gmail.com>
2021-06-28 05:03:02 -0300

    Tela de criação de salas

---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,27 @@
+import illustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg'
+import '../styles/auth.scss'
+import { Button } from '../components/button'
+
+export function NewRoom(){
+    return(
+        <div id="page-auth">
+            <aside>
+                <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas"/>
+                <strong>Toda pergunta tem uma resposta.</strong>
+                <p>Aprenda e compartilhe conhecimento com outras pessoas</p>
+            </aside>
+            <main>
+                <div className="main-content">
+                    <img src={logoImg} alt="LetMeAsk"/>
+                    <h2>Criar uma nova sala</h2>
+                    <form>
+                        <input type="text" placeholder="Nome da sala" />
+                        <Button type="submit">Criar sala</Button>
+                    </form>
+                    <p>Quer entrar em uma sala existente <a href="#">clique aqui</a></p>
+                </div>
+            </main>
+        </div>
+    )
+}
